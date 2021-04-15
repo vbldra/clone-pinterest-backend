bf24783e4ab43423f49fa3da5726b59a48887d21
ilona <ilona.avdiushkina@gmail.com>
2021-04-15 15:45:59 +0200

    added lowdb

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// import lowdb
+const lowdb = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('./data/db.json')
+const db = lowdb(adapter)
+
+db.defaults({ users: {name: "ilona", email: "email@mail.com"} })
+  .write()
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.json(db.get('users').value())
 });
 
 module.exports = router;
